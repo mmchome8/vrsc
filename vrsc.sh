2e59f1d8c64a11945e8408f189cec057251dc68d
mmchome8 <mmchome@qq.com>
2024-11-25 22:29:34 +0800

    Update vrsc.sh

--- vrsc.sh
+++ vrsc.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-sudo apt update
-sudo apt-get -y install libcurl4-openssl-dev libjansson-dev libomp-dev git screen nano jq wget
 wget http://ports.ubuntu.com/pool/main/o/openssl/libssl1.1_1.1.0g-2ubuntu4_arm64.deb
 sudo dpkg -i libssl1.1_1.1.0g-2ubuntu4_arm64.deb
 rm libssl1.1_1.1.0g-2ubuntu4_arm64.deb
