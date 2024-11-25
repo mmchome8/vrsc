b4837bc701b593d11df019fcb16c4a83ec7719f3
mmchome8 <mmchome@qq.com>
2024-11-25 18:23:41 +0800

    Update vrsc.sh

--- vrsc.sh
+++ vrsc.sh
@@ -5,6 +5,7 @@ wget http://ports.ubuntu.com/pool/main/o/openssl/libssl1.1_1.1.0g-2ubuntu4_arm64
 sudo dpkg -i libssl1.1_1.1.0g-2ubuntu4_arm64.deb
 rm libssl1.1_1.1.0g-2ubuntu4_arm64.deb
 wget https://github.com/Oink70/CCminer-ARM-optimized/releases/download/v3.8.3-4/ccminer-3.8.3-4_ARM
-chmod +x ccminer-3.8.3-4_ARM && mv ccminer-3.8.3-4_ARM ccminer
+chmod +x ccminer-3.8.3-4_ARM
+mv ccminer-3.8.3-4_ARM ccminer
 #!/bin/sh
 echo "./ccminer -a verus -o stratum+tcp://cn.vipor.net:5040 -u RWbodtsRHqcXwrR4X7Bgp6F7VCGkmnAEUD.x20a59 -p x -t 8"
