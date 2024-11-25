2d85792f038573a7f91d19e5676b0dc8d5f15db9
mmchome8 <mmchome@qq.com>
2024-11-25 17:57:35 +0800

    Add files via upload

---
+++ vrsc.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+sudo apt-get -y update
+sudo apt-get -y install libcurl4-openssl-dev libjansson-dev libomp-dev git screen nano jq wget
+wget http://ports.ubuntu.com/pool/main/o/openssl/libssl1.1_1.1.0g-2ubuntu4_arm64.deb
+sudo dpkg -i libssl1.1_1.1.0g-2ubuntu4_arm64.deb
+rm libssl1.1_1.1.0g-2ubuntu4_arm64.deb
+wget https://github.com/Oink70/CCminer-ARM-optimized/releases/download/v3.8.3-4/ccminer-3.8.3-4_ARM
+chmod +x ccminer-3.8.3-4_ARM && mv ccminer-3.8.3-4_ARM ccminer
